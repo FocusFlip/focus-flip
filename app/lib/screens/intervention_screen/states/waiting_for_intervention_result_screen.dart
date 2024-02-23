5bb45545fca292a6129816a379763ab5b368ca8b
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-02-23 14:15:55 +0100

    WaitingForInterventionResultScreen design

--- app/lib/screens/intervention_screen/states/waiting_for_intervention_result_screen.dart
+++ app/lib/screens/intervention_screen/states/waiting_for_intervention_result_screen.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../components/intervention_screen_template.dart';
 
 class WaitingForInterventionResultScreen extends StatelessWidget {
   const WaitingForInterventionResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Intervention"),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Waiting...",
-            ),
-          ],
-        )));
+    return InterventionScreenTemplate(
+        illustartion: SizedBox.shrink(),
+        titleText: "Loading...",
+        subtitleText: "Flip your Focus",
+        body: Container(
+          alignment: Alignment.center,
+          height: ScreenUtil().setHeight(100),
+          child: CircularProgressIndicator(),
+        ));
   }
 }
