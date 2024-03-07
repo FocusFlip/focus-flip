be79014611fdecc579c6585cc544426d0d764d50
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-03-07 20:01:47 +0100

    add package names for android compatibility

--- app/lib/repositories/predefined_app_list_repository.dart
+++ app/lib/repositories/predefined_app_list_repository.dart
@@ -3,10 +3,16 @@ import 'package:focus_flip/models/app.dart';
 /// We assume that the names are unique.
 class PredefinedAppListRepository {
   static const List<TriggerApp> triggerApps = [
-    TriggerApp(name: "Instagram", url: "instagram://"),
-    TriggerApp(name: "Youtube", url: "youtube://"),
+    TriggerApp(
+        name: "Instagram",
+        url: "instagram://",
+        packageName: "com.instagram.android"),
+    TriggerApp(
+        name: "Youtube",
+        url: "youtube://",
+        packageName: "com.google.android.youtube"),
     // TODO: extend the list on iOS platform
-    // TODO: check the urls and extend the list
+    // TODO: check the urls/package names and extend the list
   ];
 
   static const List<HealthyApp> healthyApps = [
